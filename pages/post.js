3e149363ceb70f1935b77a9a807a28096fca3396
Giovanni Pires da Silva <giovannipds@gmail.com>
2018-09-06 17:37:12 -0300

    Try to separate content and page definition of post page component and get an error

--- pages/post.js
+++ pages/post.js
@@ -1,10 +1,16 @@
 import {withRouter} from 'next/router'
 import Layout from '../components/MyLayout.js'
 
-const Page = withRouter((props) => (
-	<Layout>
+const Content = (props) => (
+	<div>
 		<h1>{props.router.query.title}</h1>
 		<p>This is the blog post content.</p>
+	</div>
+)
+
+const Page = withRouter((props) => (
+	<Layout>
+		<Content />
 	</Layout>
 ))
 
