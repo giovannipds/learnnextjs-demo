f2784384151cf87bb8448558088e3cac8a4b5f1c
Giovanni Pires da Silva <giovannipds@gmail.com>
2018-09-06 17:25:42 -0300

    Try the fourth! It works again

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,5 @@
 import Layout from '../lib/layout'
 
-const Page = () => (
-	<p>This is the about page</p>
-)
+const content = (<p>This is the about page</p>)
 
-export default () => (<Layout page={Page}/>)
+export default () => (<Layout page={content}/>)
