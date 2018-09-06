4abe0276eef4ec9a976e9451251e7ea0e63e2796
Giovanni Pires da Silva <giovannipds@gmail.com>
2018-09-06 17:39:21 -0300

    Move the withRouter call to the Content component and get rid of the past error

--- pages/post.js
+++ pages/post.js
@@ -1,17 +1,17 @@
 import {withRouter} from 'next/router'
 import Layout from '../components/MyLayout.js'
 
-const Content = (props) => (
+const Content = withRouter((props) => (
 	<div>
 		<h1>{props.router.query.title}</h1>
 		<p>This is the blog post content.</p>
 	</div>
-)
+))
 
-const Page = withRouter((props) => (
+const Page = (props) => (
 	<Layout>
 		<Content />
 	</Layout>
-))
+)
 
 export default Page
